422c7f5933024d5c4f581d43c3762a6c54c9b34d
Simone Zama <simone.zama@studio.unibo.it>
2021-11-10 10:26:58 +0100

    Implement OneListAcceptable.java method acceptor with anonymous class

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,15 +1,40 @@
 package it.unibo.oop.lab.nesting2;
 
+import java.util.Iterator;
+import java.util.List;
+
+import it.unibo.oop.lab.nesting2.Acceptor.ElementNotAcceptedException;
+import it.unibo.oop.lab.nesting2.Acceptor.EndNotAcceptedException;
+
 public class OneListAcceptable<T> implements Acceptable<T> {
 
-	public OneListAcceptable() {
-		// TODO Auto-generated constructor stub
+	private final List<T> list;
+	
+	public OneListAcceptable(final List<T> list) {
+		this.list = list;
 	}
 
 	@Override
 	public Acceptor<T> acceptor() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		final Acceptor<T> acceptor = new Acceptor<T>() {
+			private final Iterator<T> iterator = OneListAcceptable.this.list.iterator();
 
+			@Override
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				if (!(this.iterator.hasNext() && this.iterator.next() ==  newElement)) {
+					final ElementNotAcceptedException e = new ElementNotAcceptedException(newElement);
+					throw e;
+				}
+			}
+
+			@Override
+			public void end() throws EndNotAcceptedException {
+				if (this.iterator.hasNext()) {
+					final EndNotAcceptedException e = new EndNotAcceptedException();
+					throw e;
+				}
+			}		
+		};
+		return acceptor;
+	}
 }
