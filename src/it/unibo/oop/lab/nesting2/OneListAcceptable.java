dec5809e371c501f8a6bd5c4333a0f0057d2b406
Simone Zama <simone.zama@studio.unibo.it>
2021-11-10 09:58:17 +0100

    Create OneListAcceptable.java

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,15 @@
+package it.unibo.oop.lab.nesting2;
+
+public class OneListAcceptable<T> implements Acceptable<T> {
+
+	public OneListAcceptable() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Acceptor<T> acceptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
